d9cde6752d38acb697e813fa364aae8632dbe94b
Abderrahim El Ouali <dev.abderrahimelouali@gmail.com>
2025-06-05 15:41:19 +0100

    models and migrations

--- resources/views/components/master.blade.php
+++ resources/views/components/master.blade.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <title>Social Network | @yield('title')</title>
+    <title>Social Network | {{$title}}</title>
 </head>
 <body>
     @include('partials.nav')
 
     <main>
-        @yield('content')
+       {{$slot}}
     </main>
 
     @include('partials.footer')
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,12 @@
-@extends('layouts.master')
+    <x-master title="Home Page">
+        <h3>Welcome to home page</h3>
+        <x-users-table :users="$users" />
+        {{-- it is not recommened to use slots, use props instead --}}
+        <x-alert color="warning">warning alert</x-alert>
+        <x-alert color="primary">primary alert</x-alert>
+        <x-alert color="danger">danger alert</x-alert>
+        <x-alert color="success">success alert</x-alert>
+    </x-master>
 
-@section('title')
-    Home
-@endsection
 
-@section('content')
-    <h1>Welcome to home page</h1>
-    <x-users-table :users="$users" />
-    {{-- it is not recommened to use slots, use props instead --}}
-    <x-alert color="warning">warning alert</x-alert>
-    <x-alert color="primary">primary alert</x-alert>
-    <x-alert color="danger">danger alert</x-alert>
-    <x-alert color="success">success alert</x-alert>
-@endsection
+    
