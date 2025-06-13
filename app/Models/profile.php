945bf36ed7a749f0647b174f5ada5875ab9c8084
Abderrahim El Ouali <dev.abderrahimelouali@gmail.com>
2025-06-13 01:08:20 +0100

    24-Soft Deletes in Laravel

--- app/Models/profile.php
+++ app/Models/profile.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Profile extends Model
 {
     /** @use HasFactory<\Database\Factories\ProfileFactory> */
     use HasFactory;
+    use SoftDeletes;
     protected $fillable = ['name', 'email', 'password','image', 'bio'];
+
 }
